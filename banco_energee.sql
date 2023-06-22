aad9ef781d61bf736aa23401fb0a07179ad945d7
MarcosEnergee <129970851+MarcosEnergee@users.noreply.github.com>
2023-06-22 00:35:06 -0300

    Update banco_energee.sql

--- banco_energee.sql
+++ banco_energee.sql
@@ -41,7 +41,7 @@ INSERT INTO `distribuidoras_distribuidoras` (`id`, `nome`, `status`, `cliente_id
 -- Despejando dados para a tabela `administradores_administradores`
 --
 
-INSERT INTO `administradores_administradores` (`id`, `nome`, `end`, `fone`, `distribuidora_id`, `senha`, `uc`) VALUES
+INSERT INTO `Administradores_administradores` (`id`, `nome`, `end`, `fone`, `distribuidora_id`, `senha`, `uc`) VALUES
 (1, 'Marcos', 'end ficticio tv.44', '897577899', 1, '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '84683481000177'),
 (2, 'ENCANTO', 'nao cadastrado', '999999999', 2, '1234', '84683481000177');
 
